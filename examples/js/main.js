4a6f424acf2917a771b6559f1efa457964695c21
Victor Ribeiro <devjvribeiro@gmail.com>
2022-04-28 16:31:59 -0300

    Bumped to v1.1.0
    - Added feature to allow multiple videos at once
    - Improved example

--- examples/js/main.js
+++ examples/js/main.js
@@ -1,6 +1,5 @@
 import { yotoo } from '../../js/yotoo.js' // or `import { yotoo } from 'yotoo'` if NPM
 
-const videoContainer = document.querySelector('.video-container')
 
 /**
   * First, you must create a project in the Google Developers Console and generate a new API key.
@@ -23,20 +22,36 @@ yotoo.apiKey = 'YOUR_API_KEY_HERE' // `process.env.MIX_YOUTUBE_API_KEY` (check t
 */
 
 yotoo.get(
-  'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-  
   // First parameter is the video URL, or an object with the `url` property or an array of URLs
-  // The second is callback function when video is loaded
-  function (video) { // The video variable contains all the information about the video
-    console.log('Video loaded:', video)
+  [
+    'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+    'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+  ],
 
-    // You can do whatever you want with the video props
-    videoContainer.innerHTML =
-    /* html */`
-      <iframe
-        src="https://youtube.com/embed/${video.id}"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      />
-    `
+  // The second is callback function when video is loaded
+  function (videos) { // The video variable contains all the information about the video
+    console.log('Videos loaded:', videos)
+
+    const videoContainer = document.querySelectorAll('.video-container')
+
+    videos.forEach(video => {
+      videoContainer.forEach(item => {
+        // You can display the embedded videos
+        if (item.classList.contains('--embed')) {
+          item.insertAdjacentHTML('beforeend',
+            `<iframe src="${video.embedUrl}" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+          )
+        }
+
+        // Or show a thumbnail gallery
+        else if (item.classList.contains('--thumbs')) {
+          item.insertAdjacentHTML('beforeend',
+            `<a href="${video.url}" target="_blank">
+              <img src="${video.thumb.sd}" alt="${video.title}" title="${video.title}" />
+            </a>`
+          )
+        }
+      })
+    })
   }
 )
---
+++ js/yotoo.js
@@ -0,0 +1,137 @@
+/**
+  * YotooJS - YouTube Video Fetcher
+  * @author Victor Ribeiro <https://github.com/ojvribeiro>
+  * @licence MIT
+  */
+
+
+const yotoo = {
+  /**
+    * @param {string|Object} props - The video URL or the options object
+    * @param {function} callback - The callback function when video is loaded
+  */
+  get: (props, callback) => {
+    if (yotoo.apiKey && yotoo.apiKey !== '') {
+      let videoArray = []
+      let videoUrl
+
+      if (props instanceof Array) {
+        props.forEach(item => {
+          const videoId = getVideoId(item)
+
+          videoArray.push(videoId)
+        })
+      }
+      else {
+        if (typeof props === 'string') {
+          videoUrl = props
+
+          const videoId = getVideoId(props)
+
+          videoArray.push(videoId)
+        }
+        else {
+          if (props.url instanceof Array) {
+            props.url.forEach(item => {
+              const videoId = getVideoId(item)
+
+              videoArray.push(videoId)
+            })
+          }
+          else {
+            videoUrl = props.url
+
+            const videoId = getVideoId(props.url)
+
+            videoArray.push(videoId)
+          }
+        }
+      }
+
+      const ytapi = {
+        endpoint: 'https://www.googleapis.com/youtube/v3/videos',
+        part: 'id%2C+snippet%2C+contentDetails',
+      }
+
+      // Initialize empty idList
+      ytapi.idList = ''
+
+      for (let index in videoArray) {
+        if (index < videoArray.length) {
+          ytapi.idList += `id=${videoArray[index]}&`
+        } else {
+          ytapi.idList += `id=${videoArray[index]}`
+        }
+      }
+
+      function getVideoId(videoUrl) {
+        const id = videoUrl.replace(/(.*?)(?:youtube\.[a-z]+\/[a-z\?\&]*v[/|=]|youtu\.be\/)([0-9a-zA-Z-_]+)/g, '$2')
+        return id
+      }
+
+      yotoo.fetch(`${ytapi.endpoint}?part=${ytapi.part}&${ytapi.idList}&key=${yotoo.apiKey}`)
+
+      // Get the video info
+      .then(response => {
+        let video = []
+        const data = response.items
+
+        data.forEach(item => {
+          video.push({
+            id: item.id,
+
+            title: item.snippet.title,
+
+            description: item.snippet.description,
+
+            date: new Date(item.snippet.publishedAt),
+
+            tags: item.snippet.tags,
+
+            thumb: {
+              default: item.snippet.thumbnails.default.url,
+              sd: item.snippet.thumbnails.medium.url,
+              hd: item.snippet.thumbnails.high.url,
+            },
+
+            url: `https://www.youtube.com/?v=${item.id}`,
+            shortUrl: `https://youtu.be/${item.id}`,
+            embedUrl: `https://www.youtube.com/embed/${item.id}`,
+          })
+        })
+
+        return video
+      })
+
+      .then(video => {
+        if (typeof props === 'object') {
+          if (props.debug === true) {
+            console.log('videoProps: ', video)
+          }
+        }
+
+        if (callback) callback(video)
+      })
+
+      .catch(error => {
+        throw new Error('Ocorreu um erro ao carregar o vídeo: ', error)
+      })
+    }
+    else {
+      console.warn('API key is not set!')
+      console.info('You can get one at https://console.developers.google.com/apis/credentials.')
+    }
+  },
+
+
+  fetch: (url) => {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+      .then(response => response.json())
+      .then(data => resolve(data))
+      .catch(error => reject(error))
+    })
+  }
+}
+
+export { yotoo }
