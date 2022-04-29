2c4bca886b07bd7bddfbfa3b75a3e32b511c9e39
Victor Ribeiro <devjvribeiro@gmail.com>
2022-04-28 21:24:18 -0300

    Compiled corrected files

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,8 +4,28 @@ mix
   .setPublicPath('.')
 
   .js('./src/yotoo.js', './dist/yotoo.min.js')
+  .babel('./src/yotoo.esm.js', './dist/yotoo.esm.js')
+
   .js('./examples/js/main.js', './examples/js/build/build.js')
+
   .options({
     manifest: false,
+    terser: {
+      extractComments: false,
+
+      terserOptions: {
+        mangle: false,
+        compress: {
+          drop_console: true
+        },
+      },
+    },
   })
+
   .version()
+
+  // if not in production, disable source maps
+  .webpackConfig({
+    devtool: mix.inProduction() ? false : 'source-map',
+  })
+
